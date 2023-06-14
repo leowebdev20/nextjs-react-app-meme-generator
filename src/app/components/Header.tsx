2b2479f4c7166dbe278d9a92c7e192bf8d3c4748
leowebdev20 <serlebeiso@gmail.com>
2023-06-14 22:00:55 +0200

    page and form

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import React from "react";
+import Meme from "./Meme";
+
+export default function Header() {
+  return (
+    <>
+      <header className="header">
+        <Image
+          alt="meme logo"
+          src="/images/meme2.png"
+          className="header--image"
+          width={"70"}
+          height={"100"}
+        />
+        <h2 className="header--title">Meme Generator</h2>
+        <h4 className="header--project">React Course - Project 3</h4>
+      </header>
+      <main>
+        <div>
+          <Meme />
+        </div>
+      </main>
+    </>
+  );
+}
